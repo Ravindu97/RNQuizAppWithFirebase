cc746eda4d8c169788adebca4de5b4a79b5c7522
RavinduFernando <f.ravindu@gmail.com>
2020-07-13 13:46:21 +0530

    added fire base authentication - fixed the bugs

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Dashboard from "./components/dashboard";
+
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -14,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ database/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDakGY_-w4tz_SjNprm75fXNycTvtXFWms",
+  authDomain: "reactnativefirebase-85ca4.firebaseapp.com",
+  databaseURL: "https://reactnativefirebase-85ca4.firebaseio.com",
+  projectId: "reactnativefirebase-85ca4",
+  storageBucket: "reactnativefirebase-85ca4.appspot.com",
+  messagingSenderId: "601731250031",
+  appId: "1:601731250031:web:e92e0c4053bf7ef7949a9a",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
